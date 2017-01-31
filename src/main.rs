903c5d6c79beafd85254c14271bd95bf371c7423
René Perschon <rperschon85@gmail.com>
2017-01-31 21:02:57 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+extern crate termion;
+
+mod game_of_life;
+
+use std::thread;
+use std::time;
+use game_of_life::GameOfLife;
+
+fn main() {
+    let mut gof = GameOfLife::new(30, 30);
+    // Blinker
+    gof.awake(14, 15);
+    gof.awake(15, 15);
+    gof.awake(16, 15);
+
+    loop {
+        clear_screen();
+        print_gof(&gof);
+        gof.step();
+        thread::sleep(time::Duration::from_millis(500));
+    }
+}
+
+fn print_gof(gof: &GameOfLife) {
+    for _ in 0..gof.width() + 2 {
+        print!("-");
+    }
+    println!("");
+
+    for y in 0..gof.height() {
+        print!("|");
+        for x in 0..gof.width() {
+            print!("{}", gof.get(x, y).to_char());
+        }
+        println!("|");
+    }
+
+    for _ in 0..gof.width() + 2 {
+        print!("-");
+    }
+    println!("");
+}
+
+fn clear_screen() {
+    print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+}
